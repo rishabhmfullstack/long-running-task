583381d03c22b4220a4a0600035e5e5deb0270f0
rishabhmfullstack <rishabhmfullstack@gmail.com>
2025-01-30 20:33:59 +0530

    modify struct

--- models/models.go
+++ models/models.go
@@ -1,7 +1,12 @@
 package models
 
+import "time"
+
 type Task struct {
-	ID     int    `json:"id"`
-	Status string `json:"status"`
-	Result string `json:"result,omitempty"`
+	ID        int       `json:"id"`
+	Status    string    `json:"status"`
+	Result    string    `json:"result,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt string    `json:"deleted_at,omitempty"`
 }
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,11 +1,12 @@
-package tasks
+package models
 
-import (
-	"task-api/database"
-	"time"
-)
+import "time"
 
-func PerformLongRunningTask(id int) {
-	time.Sleep(10 * time.Second)
-	database.GetDB().Exec("UPDATE tasks SET status = ?, result = ? WHERE id = ?", "completed", "Task completed successfully", id)
+type Task struct {
+	ID        int        `json:"id"`
+	Status    string     `json:"status"`
+	Output    string     `json:"output"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
 }
